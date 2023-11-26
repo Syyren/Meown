fce8727d91b65225c64c22bbd2a2d76bdda12bab
Syyren <129496054+Syyren@users.noreply.github.com>
2023-11-25 18:04:45 -0700

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".slider").bxSlider({
+        mode: 'fade',
+        pager: false,
+    });
+});
+
+lightbox.option({
+       'wrapAround': true,
+       'albumLabel': ""
+});
